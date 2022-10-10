82ec41a15cf0a7df218c74b5d35d66dd0f1b488d
jaykim-github <jaykeem713@gmail.com>
2022-10-10 23:19:10 +0900

    2022.10.10 : sqlite와 테스트, maxNum 가져오는 테스트 완료

--- publicWifi/src/main/java/service/ApiExplorer.java
+++ publicWifi/src/main/java/service/ApiExplorer.java
@@ -6,7 +6,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -15,13 +18,45 @@ import org.json.simple.parser.ParseException;
 
 
 public class ApiExplorer {
+    static ApiExplorer ae = new ApiExplorer();
+	
 	public static void main(String[] args) throws IOException {
+		//연결 후 최대값 가져오기
+		int maxNum = ae.getWifiMaxNum();
+		System.out.println(maxNum);
+		int start= 1;
+		int end = 0;
+		
+		while(maxNum >1000) {
+		
+			if(maxNum > 1000) {
+				end = end + 1000;
+				maxNum = maxNum - 1000;
+			}else {
+				end = maxNum;
+			}
+			//여기서 db에 저장
+			System.out.println(start + " " + end);
+			System.out.println();
+			
+			//start 초기화
+			start = end+1;
+		}
+		maxNum = maxNum + start;
+		System.out.println(start + " " + maxNum);
+		System.out.println();
+		
+		ae.connectDB();
+	}
+	
+	//start to end 가져오기
+	public String getWifiInfo(int start, int end) throws IOException {
 		StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088");
 		urlBuilder.append("/" + URLEncoder.encode("4462734865646d623130327563504b5a", "UTF-8"));
 		urlBuilder.append("/" + URLEncoder.encode("json", "UTF-8"));
 		urlBuilder.append("/" + URLEncoder.encode("TbPublicWifiInfo", "UTF-8"));
-		urlBuilder.append("/" + URLEncoder.encode("1", "UTF-8"));
-		urlBuilder.append("/" + URLEncoder.encode("1000", "UTF-8"));
+		urlBuilder.append("/" + URLEncoder.encode(Integer.toString(start), "UTF-8"));
+		urlBuilder.append("/" + URLEncoder.encode(Integer.toString(end), "UTF-8"));
 		//urlBuilder.append("/" + URLEncoder.encode("20220301", "UTF-8"));
 		URL url = new URL(urlBuilder.toString());
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -38,39 +73,97 @@ public class ApiExplorer {
 		}
 		StringBuilder sb = new StringBuilder();
 		
-//		while(rd.ready()) {
-//			sb.append(rd.readLine());
-//		}
-		String line;
-		while ((line = rd.readLine()) != null) {
-			sb.append(line);
+		while(rd.ready()) {
+			sb.append(rd.readLine());
 		}
 		
 		rd.close();
 		conn.disconnect();
-		System.out.println(sb.toString());
 		
 		String result = sb.toString();
 		
+		return result;	
+	}
+	
+	// 최대값 가져오기
+	static int getWifiMaxNum() throws IOException {
+		
+		String result = ae.getWifiInfo(1,1);
+		
+		Long total_countL = 0L;
+    	try {
+    		JSONParser jsonParser = new JSONParser();
+			JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
+			JSONObject tbpublicWifiInfo = (JSONObject)jsonObject.get("TbPublicWifiInfo");
+
+			total_countL = (Long)tbpublicWifiInfo.get("list_total_count");
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+    	
+    	int total_count = total_countL.intValue();
+    	return total_count;
+	}
+	
+	//DB 연결 ...? 이게 필요한지 의문 어쩄든
+	 public static void connectDB()
+	  {
+	      Connection c = null;
+	        try {
+	          Class.forName("org.sqlite.JDBC");
+	          c = DriverManager.getConnection("jdbc:sqlite:publicWifi.db");
+	        } catch ( Exception e ) {
+	          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	          System.exit(0);
+	        }
+	        System.out.println("Opened database successfully");
+	  }
+	 
+	 
+	
+	//Json으로 변형하여 DB에 넣기
+	public int insertDB(String result) {
+		Connection c = null;
+		Statement stmt = null;
 		
     	try {
+    		  //DB 연결
+    		  Class.forName("org.sqlite.JDBC");
+              c = DriverManager.getConnection("jdbc:sqlite:publicWifi.db");
+              c.setAutoCommit(false);
+              System.out.println("Opened database successfully");
+     
+              stmt = c.createStatement();
+            
+            //Json으로 변형 후 sql문 insert
     		JSONParser jsonParser = new JSONParser();
 			JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
 			JSONObject tbpublicWifiInfo = (JSONObject)jsonObject.get("TbPublicWifiInfo");
 			JSONArray publicwifiInfo = (JSONArray)tbpublicWifiInfo.get("row");
 			
+			
 			String str = "";
+			String sql = "";
 			for(int i = 0; i<publicwifiInfo.size(); i++) {
-				JSONObject X_SWIFI_MGR_NO = (JSONObject)publicwifiInfo.get(i);
+				JSONObject rowData = (JSONObject)publicwifiInfo.get(i);
+			
+				str += rowData.get("X_SWIFI_MGR_NO") + "\n";
 				
-				str += X_SWIFI_MGR_NO.get("X_SWIFI_MGR_NO") + "\n";
+				sql  = "INSERT INTO wifiInfo () " +  " ";
+				stmt.executeUpdate(sql);
 			}
 			
-			System.out.print(str);
+           
+            stmt.close();
+            c.commit();
+            c.close();
+			//System.out.print(str);
 			
-		} catch (ParseException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		
+		return 1;
 	}
 }
